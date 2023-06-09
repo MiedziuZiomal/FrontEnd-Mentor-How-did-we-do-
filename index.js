07a99e98125c6eb2219c35feb91ef9f8e96bf3d7
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-09 16:09:47 +0200

    added back card, and some javascript

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const ratePoints = document.querySelectorAll('.rate-box__ratings-item')
-console.log(ratePoints);
-
+const ratePoints = document.querySelectorAll('.rate-box__ratings-item');
+const thankYou = document.querySelector('.thank-you');
+const front = document.querySelector('.front-card');
+const submitBtn = document.querySelector('.submit');
+const showRating = document.querySelector('.selected-option');
 
 const chosenOptionHighlight = (e) => {
-    const targetElement = e.target;
-    const isActive = targetElement.classList.contains('active');
-  
-    if (isActive) {
-      targetElement.classList.remove('active');
-    } else {
-      const alreadyActive = document.querySelector('.rate-box__ratings-item.active');
-      if (alreadyActive) {
-        alreadyActive.classList.remove('active');
-      }
-      targetElement.classList.add('active');
-    }
-  };
-  
+	const targetElement = e.target;
+	const alreadyActive = document.querySelector(
+		'.rate-box__ratings-item.active'
+	);
+	if (alreadyActive) {
+		alreadyActive.classList.remove('active');
+	}
+	targetElement.classList.add('active');
+};
+
+const showThankYou = () => {
+	front.style.display = 'none';
+	thankYou.style.display = 'block';
+};
+
+const assignValue = (e) => {
+	let x;
+	x = e.target.textContent;
+	showRating.textContent = x;
+};
 
 
-
-
-const rateOptionListeners = ()=>{
-    ratePoints.forEach(element => {
-        element.addEventListener('click', chosenOptionHighlight)
-    });
-}
-
-rateOptionListeners()
+ratePoints.forEach((element) => {
+	element.addEventListener('click', chosenOptionHighlight);
+	element.addEventListener('click', assignValue);
+});
+submitBtn.addEventListener('click', showThankYou);
